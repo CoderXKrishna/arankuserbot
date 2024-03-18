480a681bcefc8baf8a9abd6245c39570849e5b01
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2024-03-18 05:58:34 -0700

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -61,7 +61,7 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
     ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**✮ MY BOT IS RUNNING SUCCESSFULLY ✮**"
-    arank_IMG = gvarstatus("ALIVE_PIC")
+    arank_IMG = Config.ALIVE_PIC
     caption = arank_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
         ANIME=ANIME,
