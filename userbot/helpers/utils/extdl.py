63ad9f14d18a6eb71268b929a5d0a7d3267efcf2
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2024-03-31 06:34:36 -0700

    Update extdl.py

--- userbot/helpers/utils/extdl.py
+++ userbot/helpers/utils/extdl.py
@@ -14,5 +14,5 @@ def install_pip(pipfile):
     print(f"installing {pipfile}")
     pip_cmd = ["pip", "install", f"{pipfile}"]
     process = Popen(pip_cmd, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communiaranke()
+    stdout, stderr = process.communicate()
     return stdout
